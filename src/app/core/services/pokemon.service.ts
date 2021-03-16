028f2c6ae119bd89fbaadf31b88764c748284aab
sebaslopezp <seblopezp@gmail.com>
2021-03-16 17:45:30 -0300

    ADD: metodo getPokemon y getImage

--- src/app/core/services/pokemon.service.ts
+++ src/app/core/services/pokemon.service.ts
@@ -1,18 +1,23 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
-import * as PokemonModel from '../models/pokemon.model';
+import * as Pokemon from '../models/pokemon.model';
 import { HttpClient } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
+
 @Injectable({
   providedIn: 'root',
 })
 export class PokemonService {
   private readonly url = environment.API_SERVER_URL;
-
+  private imagePath = environment.API_SERVER_IMAGE;
   constructor(private http: HttpClient) {}
 
-  getPokemon(): Observable<PokemonModel.Pokemon> {
-    return this.http.get<PokemonModel.Pokemon>(`${this.url}`);
+  getPokemon(): Observable<Pokemon.Data> {
+    return this.http.get<Pokemon.Data>(`${this.url}`);
+  }
+
+  getImage(id: number): any {
+    return this.http.get(`${this.imagePath}/` + id + '.png');
   }
 }
