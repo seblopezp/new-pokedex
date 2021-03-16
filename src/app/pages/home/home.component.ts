1e593afcbe546ca6a23fa99f34e4a025fb6d1788
sebaslopezp <seblopezp@gmail.com>
2021-03-16 17:43:38 -0300

    UPDATE: se muestran card pokemon

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,23 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 import { PokemonService } from '../../core/services/pokemon.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
-  pokemon = {};
+export class HomeComponent implements OnInit, OnDestroy {
+  pokemon: any;
+  destroy$: Subject<boolean> = new Subject<boolean>();
   constructor(private pokemonService: PokemonService) {}
 
   ngOnInit(): void {
-    this.getPokemon();
+    if (localStorage.getItem('dataSource')) {
+      this.getPokemon();
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+    this.destroy$.complete();
   }
 
   getPokemon() {
-    this.pokemonService.getPokemon().subscribe((data) => {
-      this.pokemon = data;
-      console.log('datos', this.pokemon);
-      console.log('tipo', typeof this.pokemon);
-    });
+    this.pokemonService
+      .getPokemon()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((data) => {
+        this.pokemon = data.results;
+      });
   }
 }
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-
+import { PokemonCardModule } from '../pokemon-card/pokemon-card.module';
 @NgModule({
-  imports: [CommonModule, HomeRoutingModule],
+  imports: [CommonModule, HomeRoutingModule, PokemonCardModule],
   declarations: [HomeComponent],
 })
 export class HomeModule {}
