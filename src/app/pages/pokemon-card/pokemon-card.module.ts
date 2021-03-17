0eac926dd5b56b993260333b92fb8b1721168098
sebaslopezp <seblopezp@gmail.com>
2021-03-17 11:44:45 -0300

    UPDATE: se importa modulo shared

--- src/app/pages/pokemon-card/pokemon-card.module.ts
+++ src/app/pages/pokemon-card/pokemon-card.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PokemonCardComponent } from './pokemon-card.component';
-
+import { SharedModule } from '../../shared/shared.module';
 @NgModule({
   declarations: [PokemonCardComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, SharedModule],
   exports: [PokemonCardComponent],
 })
 export class PokemonCardModule {}
