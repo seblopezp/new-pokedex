4ee1ff94b22f0cf3ce19e67e2255d3794083c937
sebaslopezp <seblopezp@gmail.com>
2021-03-17 11:54:08 -0300

    ADD: se obtiene imagen pokemon

--- src/app/pages/pokemon-card/pokemon-card.component.ts
+++ src/app/pages/pokemon-card/pokemon-card.component.ts
@@ -1,26 +1,30 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { PokemonInfo } from 'src/app/core/models/pokemon.model';
 import { PokemonService } from '../../core/services/pokemon.service';
-
 @Component({
   selector: 'app-pokemon-card',
   templateUrl: './pokemon-card.component.html',
   styleUrls: ['./pokemon-card.component.scss'],
 })
 export class PokemonCardComponent implements OnInit {
-  @Input() pokemonName?: string;
+  @Input() pokemonName?: any;
+  pokemonImage: any;
+  pokemonInfo: PokemonInfo = new PokemonInfo();
   constructor(private pokemonService: PokemonService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getPokemonInfo();
+  }
 
   getPokemonInfo() {
-    //   this.pokemonService
-    //     .getPokemonbyName(this.pokemonInfo['name'])
-    //     .subscribe((data: any) => {
-    //       this.pokemonImage =
-    //         'https://pokeres.bastionbot.org/images/pokemon/' +
-    //         data['id'] +
-    //         '.png';
-    //     });
-    // });
+    this.pokemonService
+      .getPokemonbyName(this.pokemonName)
+      .subscribe((data: any) => {
+        this.pokemonInfo = data;
+        this.pokemonImage =
+          'https://pokeres.bastionbot.org/images/pokemon/' +
+          data['id'] +
+          '.png';
+      });
   }
 }
