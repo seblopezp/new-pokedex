b6a7f21a3fb63ca4e43f96a79b6b37a0067402f2
sebaslopezp <seblopezp@gmail.com>
2021-04-28 18:24:52 -0400

    UPDATE: using pagination component

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,8 +3,14 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { PokemonCardModule } from '../pokemon-card/pokemon-card.module';
+import { PaginationModule } from '../pagination/pagination.module';
 @NgModule({
-  imports: [CommonModule, HomeRoutingModule, PokemonCardModule],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    PokemonCardModule,
+    PaginationModule,
+  ],
   declarations: [HomeComponent],
 })
 export class HomeModule {}
