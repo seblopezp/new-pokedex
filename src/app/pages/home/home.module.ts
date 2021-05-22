b8b4ca36139a9a02d161b461a4756433a9ab3daa
sebaslopezp <seblopezp@gmail.com>
2021-05-21 22:53:04 -0400

    FIX: Se corrige error pagination

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,14 +3,8 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { PokemonCardModule } from '../pokemon-card/pokemon-card.module';
-import { PaginationModule } from '../pagination/pagination.module';
 @NgModule({
-  imports: [
-    CommonModule,
-    HomeRoutingModule,
-    PokemonCardModule,
-    PaginationModule,
-  ],
+  imports: [CommonModule, HomeRoutingModule, PokemonCardModule],
   declarations: [HomeComponent],
 })
 export class HomeModule {}
